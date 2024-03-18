42683e8b945628dfa26d90143c26194a9a931574
Raul Wolters <56522208+smups@users.noreply.github.com>
2024-03-18 10:47:27 +0100

    added angular on trajecotyr plots

--- angular.py
+++ angular.py
@@ -88,6 +88,22 @@ np.save(f"./out/{model}_eta_H.npy", eta_H)
 np.save(f"./out/{model}_delta.npy", delta)
 np.save(f"./out/{model}_omega.npy", omega)
 
+#run analysis on trajectory
+tx = np.load('./trajectories/angular_phix.npy')
+ty = np.load('./trajectories/angular_phiy.npy')
+trajectory = np.column_stack((tx, ty))
+
+consistency, epsilon_V, epsilon_H, eta_H, delta, omega = anguelova.complete_analysis_on_trajectory(
+  args,
+  trajectory
+)
+np.save(f"./out/{model}_ot.npy", consistency)
+np.save(f"./out/{model}_ot_epsilon_V.npy", epsilon_V)
+np.save(f"./out/{model}_ot_epsilon_H.npy", epsilon_H)
+np.save(f"./out/{model}_ot_eta_H.npy", eta_H)
+np.save(f"./out/{model}_ot_delta.npy", delta)
+np.save(f"./out/{model}_ot_omega.npy", omega)
+
 #run Anguelova's original condition
 consistency_old = anguelova.consistency_only_old(
   args,
